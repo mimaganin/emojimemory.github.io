47406d7af29b1af103476d41c741cc0c7c1da8ae
Milena Maganin <mimaganin@gmail.com>
2022-07-15 00:04:13 +0100

    rename js file

--- js/app.js
+++ js/app.js
@@ -40,27 +40,3 @@ reparteTarjetas();
 document.querySelectorAll(".tarjeta").forEach(function(elemento){
     elemento.addEventListener("click", descobrir)
 });
-
-
-  // EJERCICIO: recorre el array de tarjetasBarajadas y para cada
-  // elemento, crea un div (cárgalo en una variable) y declara que su
-  // innerHTML es el div con la clase "tarjeta" y en el interior
-  // del mismo, un div con la clase "tarjeta__contenido".
-  // Añade además el valor del elemento como contenido de la tarjeta
-
-  // EJERCICIO: una vez has creado la tarjeta, y todavía dentro del
-  // loop, añade la tarjeta generada como "child" del nodo mesa.
-
-  // BONUS: podrías hacer que las tarjetas solo se repartieran cuando
-  // se apriete un botón que ponga "repartir cartas"
-
-
-// function descubrir() {
-//   this.classList.add("descubierta");
-// }
-
-
-
-// document.querySelectorAll(".tarjeta").forEach(function(elemento) {
-//   elemento.addEventListener("click", descubrir);
-// });
